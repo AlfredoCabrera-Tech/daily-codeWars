d392ebb79d0561cde25604e77e1dd4d4a3b74cb9
Alfredo Cabrera <alfredocabrera.tech@gmail.com>
2022-07-07 23:59:56 -0400

    07-07-2022 kata done

---
+++ 07-07-2022/README.md
@@ -0,0 +1,15 @@
+# Calculate BMI (8 kyu)
+
+Write function bmi that calculates body mass index (bmi = weight / height2).
+
+```JavaScript
+if bmi <= 18.5 return "Underweight"
+
+if bmi <= 25.0 return "Normal"
+
+if bmi <= 30.0 return "Overweight"
+
+if bmi > 30 return "Obese"
+```
+
+#FUNDAMENTALS #JavaScript
---
+++ 07-07-2022/main.js
@@ -0,0 +1,19 @@
+function bmi(weight, height) {
+  let value = (weight/(height*height))
+  /* if(value <= 18.5){
+    console.log("under")
+    // return "Underweight"
+  } if(value <= 25.0){
+    console.log("norm")
+    // return "Normal"
+  } if(value <= 30.0){
+    console.log("over")
+    // return "Overweight"
+  } if(value > 30){
+    console.log("ob")
+    // return "Obese"
+  } */
+}
+
+console.log((80/(1.80*1.80)))
+bmi(80, 1.80)
