724bf7b40f9abb0a3f87aead87f9b037f75b4791
Alfredo Cabrera <alfredocabrera.tech@gmail.com>
2022-07-08 00:02:36 -0400

    07-07-2022 kata done

--- 07-07-2022/main.js
+++ 07-07-2022/main.js
@@ -1,19 +1,12 @@
 function bmi(weight, height) {
   let value = (weight/(height*height))
-  /* if(value <= 18.5){
-    console.log("under")
-    // return "Underweight"
+  if(value <= 18.5){
+    return "Underweight"
   } if(value <= 25.0){
-    console.log("norm")
-    // return "Normal"
+    return "Normal"
   } if(value <= 30.0){
-    console.log("over")
-    // return "Overweight"
+    return "Overweight"
   } if(value > 30){
-    console.log("ob")
-    // return "Obese"
-  } */
+    return "Obese"
+  }
 }
-
-console.log((80/(1.80*1.80)))
-bmi(80, 1.80)
