0a2794599eed8ff5fbd217780904d5801591ca9e
Alfredo Cabrera <alfredocabrera.tech@gmail.com>
2022-07-24 21:57:29 -0400

    07-24-2022 kata done

--- 07-24-2022/main.js
+++ 07-24-2022/main.js
@@ -1,14 +1,9 @@
 function removeEveryOther(arr){
   let oddArr = []
-  
   for (let i=0; i<arr.length; i++){
     if(i%2 === 0){
       oddArr.push(arr[i])
     }
-    //return arr
   }
-  console.log(oddArr)
+  return oddArr
 }
-
-removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-removeEveryOther(['Hello', 'Goodbye', 'Hello Again'])
