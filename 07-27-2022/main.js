4ebbd22031073d5dd4c0f77afc9044008e718ad2
Alfredo Cabrera <alfredocabrera.tech@gmail.com>
2022-07-27 17:28:36 -0400

    07-27-2022 kata done

---
+++ 07-27-2022/main.js
@@ -0,0 +1,10 @@
+let number = function(array){
+  //your awesome code here
+  let count = []
+
+  for (let i = 0; i<array.length; i++){
+    count.push(`${i+1}: ${array[i]}`)
+  }
+  //return count
+  console.log(count)
+}
