c6b072650791603c2ac9aa620efbaf1133db6f2b
Alfredo Cabrera <alfredocabrera.tech@gmail.com>
2022-09-04 22:57:13 -0400

    09-04-2022 kata done

---
+++ 09-04-2022/main.js
@@ -0,0 +1,38 @@
+function titleCase(title, minorWords='') {
+  let titleArr = title.toLowerCase().split(' ')
+  let minorArr = minorWords.toLowerCase().split(' ')
+  
+  
+  if(title!=''){
+    titleArr[0] = titleArr[0][0].toUpperCase() + titleArr[0].slice(1)
+    let titleArrCap = titleArr.map(word => {
+      
+      if(minorWords!=''){
+        for(let i=0; i<minorArr.length;i++) {
+          
+          if(!minorArr.includes(word)){
+            word = word[0].toUpperCase() + word.slice(1)
+            return word
+          } if(minorArr.includes(word)){
+            return word
+          }
+
+          return word
+        }
+
+      } else{
+        word = word[0].toUpperCase() + word.slice(1)
+        return word
+      }
+      
+    })
+    return titleArrCap.join(' ')
+  } else{
+    return ''
+  }
+}
+
+titleCase('a clash of KINGS', 'a an the of')
+// titleCase('THE WIND IN THE WILLOWS', 'The In')
+titleCase('the quick brown fox')
+titleCase('')
