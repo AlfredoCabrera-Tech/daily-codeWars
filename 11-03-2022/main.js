e2ed43f5a6c9b5777e152f49901b16dab3a2cc3a
Alfredo Cabrera <alfredocabrera.tech@gmail.com>
2022-11-03 19:34:50 -0400

    11-03-2022 kata done

---
+++ 11-03-2022/main.js
@@ -0,0 +1,14 @@
+const basicTest = require('../basicTest')
+
+function twoSort(s) {
+  let sSorted = s.sort()
+  let first = sSorted[0]
+
+  return first.split('').join('***')
+
+}
+
+// twoSort(["turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones"])
+
+basicTest(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]), 'b***i***t***c***o***i***n' );
+basicTest(twoSort(["turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones"]), 'a***r***e');
